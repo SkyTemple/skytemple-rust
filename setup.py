a08aeaa7b8d9d4e8f3ae2a3177f03236af695813
Parakoopa <parakoopa@live.de>
2021-03-07 14:01:33 +0100

    Trying to get Github build to work

--- setup.py
+++ setup.py
@@ -27,7 +27,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
     ],
     # rust extensions are not zip safe, just like C-extensions.
     zip_safe=False,
