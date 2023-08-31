57eaaf2e4d0f92a2d3d3d97d691048505301dc80
Capypara <hello@capypara.de>
2023-08-31 13:39:19 +0200

    DPCI implementation

--- src/st_dpci.rs
+++ src/st_dpci.rs
@@ -17,18 +17,78 @@
  * along with SkyTemple.  If not, see <https://www.gnu.org/licenses/>.
  */
 use crate::bytes::StBytes;
+use crate::image::tiled::TiledImage;
+use crate::image::tilemap_entry::TilemapEntry;
+use crate::image::{In256ColIndexedImage, InIndexedImage, IndexedImage, PixelGenerator};
 use crate::python::*;
+
+pub const DPCI_TILE_DIM: usize = 8;
 
 #[pyclass(module = "skytemple_rust.st_dpci")]
 #[derive(Clone)]
-pub struct Dpci {}
+pub struct Dpci {
+    #[pyo3(get, set)]
+    pub tiles: Vec<StBytes>,
+}
 
 #[pymethods]
 impl Dpci {
-    #[allow(unused_variables)]
     #[new]
     pub fn new(data: StBytes) -> PyResult<Self> {
-        todo!()
+        let tiles = data
+            .chunks(DPCI_TILE_DIM * DPCI_TILE_DIM / 2)
+            .map(StBytes::from)
+            .collect(); // / 2 because 4bpp
+        Ok(Self { tiles })
+    }
+
+    /// Convert all individual tiles of the DPCI into one image.
+    /// The image contains all tiles next to each other, the image width is tile_width tiles.
+    /// The resulting image has one large palette with all palettes merged together.
+    ///
+    /// palettes is a list of 16 16 color palettes.
+    /// The tiles are exported with the first palette in the list of palettes.
+    /// The result image contains a palette that consists of all palettes merged together.
+    #[args(width_in_mtiles = "20", palette_index = "0")]
+    pub fn tiles_to_pil(
+        &self,
+        palettes: Vec<Vec<u8>>,
+        width_in_tiles: usize,
+        palette_index: u8,
+    ) -> IndexedImage {
+        let tilemap = (0..self.tiles.len()).map(|i| TilemapEntry(i, false, false, palette_index));
+        let width = width_in_tiles * DPCI_TILE_DIM;
+        let height =
+            (((self.tiles.len()) as f32 / width_in_tiles as f32).ceil()) as usize * DPCI_TILE_DIM;
+        TiledImage::tiled_to_native(
+            tilemap,
+            PixelGenerator::tiled4bpp(&self.tiles[..]),
+            palettes.iter().flat_map(|x| x.iter().copied()),
+            DPCI_TILE_DIM,
+            width,
+            height,
+            1,
+        )
+    }
+
+    /// Imports tiles that are in a format as described in the documentation for tiles_to_pil.
+    pub fn pil_to_tiles(&mut self, image: In256ColIndexedImage, py: Python) -> PyResult<()> {
+        let image = image.extract(py)?;
+        let w = image.0 .1;
+        let h = image.0 .2;
+        let (tiles, _) = TiledImage::native_to_tiled_seq(image, DPCI_TILE_DIM, w, h)?;
+        self.tiles = tiles.into_iter().map(|x| x.0.into()).collect();
+        Ok(())
+    }
+
+    /// Replace the tiles.
+    /// If contains_null_tile is false, the null tile is added to the list, at the beginning.
+    #[args(contains_null_tile = "false")]
+    pub fn import_tiles(&mut self, mut tiles: Vec<StBytes>, contains_null_tile: bool) {
+        if !contains_null_tile {
+            tiles.insert(0, vec![0; DPCI_TILE_DIM * DPCI_TILE_DIM / 2].into());
+        }
+        self.tiles = tiles;
     }
 }
 
@@ -42,9 +102,17 @@ impl DpciWriter {
     pub fn new() -> Self {
         Self
     }
-    #[allow(unused_variables)]
+
     pub fn write(&self, model: Py<Dpci>, py: Python) -> PyResult<StBytes> {
-        todo!()
+        Ok(StBytes::from(
+            model
+                .borrow(py)
+                .tiles
+                .iter()
+                .flat_map(|x| &x.0)
+                .copied()
+                .collect::<Vec<_>>(),
+        ))
     }
 }
 
